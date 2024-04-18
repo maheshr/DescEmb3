caaf8dd4322fbb99a6bd52539401e4e41e32b923
Mahesh Ramchandani <mahesh.ramchandani@gmail.com>
2024-04-17 21:08:08 -0500

    Added param for model path

--- descemb_mimic_vc_predict.cmd
+++ descemb_mimic_vc_predict.cmd
@@ -1,8 +1,8 @@
 @echo off
 set DST_PATH=C:\gatech\bd4h\p1\d_out
-set ROOT_PATH=c:\gatech\bd4h\p1\DescEmb3
 
 REM Get the first parameter passed in
 set LABEL=%1
+set MODEL_PATH=%2
 
-python main.py --input_path %DST_PATH% --model ehr_model --embed_model descemb_bert --pred_model rnn --src_data mimiciii --ratio 100 --value_mode VC --task %LABEL% --load_pretrained_weights --model_path %ROOT_PATH%\descemb_mimic_vc.pt
+python main.py --input_path %DST_PATH% --model ehr_model --embed_model descemb_bert --pred_model rnn --src_data mimiciii --ratio 100 --value_mode VC --task %LABEL% --load_pretrained_weights --model_path %MODEL_PATH%
